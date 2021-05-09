50b27bd97720e5b0b1b0f48879dd30a2b21e56c2
Yeum Gu Whan <duarbdhks@gmail.com>
2021-05-09 17:37:00 +0900

    initialize Dev

---
+++ src/user/user.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { TestModule } from '../test/test.module';
+
+@Module({
+  imports: [TestModule],
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class UserModule {}
